b49f15f031da164af899c9c0b96220a34f3a9b3e
JakeF <jakefawcett2298@hotmail.co.uk>
2023-02-06 08:14:55 +0000

    updated hooks, added comment to azure-bicep-build.sh and made executable

--- azure-bicep-build.sh
+++ azure-bicep-build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# Script to find bicep files and run az bicep build to validate, then remove any outputs produced
+
 find . -name \*.bicep -exec az bicep build --file {} --outfile pre-commit-temp.json \;
 rm pre-commit-temp.json
