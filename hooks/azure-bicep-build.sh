beb1cac4b147741b9c7bff25d1d5d1fa438f7efd
JakeF <jakefawcett2298@hotmail.co.uk>
2023-02-06 22:29:24 +0000

    removed escaped from slashes

--- hooks/azure-bicep-build.sh
+++ hooks/azure-bicep-build.sh
@@ -2,5 +2,5 @@
 # Script to find bicep files and run az bicep build to validate, then remove any outputs produced
 set -eo pipefail
 
-find . -name \\*.bicep -exec az bicep build --file {} --outfile pre-commit-temp.json \\;
+find . -name \*.bicep -exec az bicep build --file {} --outfile pre-commit-temp.json \;
 rm pre-commit-temp.json
