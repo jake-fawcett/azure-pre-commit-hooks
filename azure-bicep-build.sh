965f59ac7b0c911e107083a64045cb7115b26b50
JakeF <jakefawcett2298@hotmail.co.uk>
2023-02-06 21:52:18 +0000

    updated shebang

--- azure-bicep-build.sh
+++ azure-bicep-build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Script to find bicep files and run az bicep build to validate, then remove any outputs produced
 
 find . -name \*.bicep -exec az bicep build --file {} --outfile pre-commit-temp.json \;
