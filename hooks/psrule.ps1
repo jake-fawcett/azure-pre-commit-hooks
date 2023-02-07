2a575d567b2c36e83869e82683aeab81a097cb7d
JakeF <jakefawcett2298@hotmail.co.uk>
2023-02-07 20:59:40 +0000

    removed psrule install

--- hooks/psrule.ps1
+++ hooks/psrule.ps1
@@ -1,7 +1,4 @@
 #!/usr/bin/env pwsh
-Write-Host "Installing PSRule"
-Install-Module -Name 'PSRule.Rules.Azure' -Scope CurrentUser -Force -Confirm:$false;
-
 $Env:PSRULE_AZURE_BICEP_PATH = '/usr/local/bin/bicep';
 
 Write-Host "Asserting PSRule"
