92439861d9e95d323c153de17c31ac7e2c6c1f9c
JakeF <jakefawcett2298@hotmail.co.uk>
2023-02-08 21:41:15 +0000

    updated deletion of build arm, moved samples into folders for organization

--- hooks/azure-arm-build.sh
+++ hooks/azure-arm-build.sh
@@ -2,5 +2,4 @@
 # Script to find json (arm) files and run az bicep decompile to validate, then remove any outputs produced
 set -eo pipefail
 
-find . -name \*.bicep -exec az bicep decompile --file {} --outfile pre-commit-temp.bicep \;
-rm pre-commit-temp.bicep
+find . -name \*.json -exec az bicep decompile --file {} \; -exec sh -c 'rm ${1%????}bicep' sh {} \;
